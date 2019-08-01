f57d5a42dc8d416390886b7c7c38f18aadd7b48a
LMBoyle <boylelukem@gmail.com>
2019-08-01 12:01:06 -0400

    added array with some strings. Psedocoded js

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// VARIABLES =================================
+var topics = ["Rick and Morty", "Greys Anatomy", "Orange Is The New Black", "Stranger Things", "Supernatural", "The Simpsons", "Friends", "How I Met Your Mother", "Teen Wolf", "Vikings", "My Name Is Earl", "White Collar", "American Horror Story", "The Great British Baking Show"]
+
+// FUNCTIONS =================================
+  // TODO On load, create and display buttons on screen
+
+  // TODO On submit of topic, create new button
+
+  // TODO On button click, run ajax and get 10 gifs
+
+  // TODO Show 10 static gifs
+
+  // TODO Show rating under gif
+
+  // TODO On gif click, animate.
+
+  // TODO On second gif click, unanimate
+
+
+// CALL FUNCTIONS ============================
