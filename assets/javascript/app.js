c33b9970bb48e41666a0ffce644a519557458daa
LMBoyle <boylelukem@gmail.com>
2019-08-01 22:49:32 -0400

    added function to show 10 more gifs on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // VARIABLES =================================
-var topics = ["Rick and Morty", "Greys Anatomy", "Orange Is The New Black", "Stranger Things", "Supernatural", "The Simpsons", "Friends", "How I Met Your Mother", "Teen Wolf", "Vikings", "My Name Is Earl", "White Collar", "American Horror Story", "The Great British Baking Show"]
-
+var topics = ["Rick and Morty", "Greys Anatomy", "Orange Is The New Black", "Stranger Things", "Supernatural", "The Simpsons", "Friends", "How I Met Your Mother", "Teen Wolf", "Vikings", "My Name Is Earl", "White Collar", "American Horror Story", "The Great British Baking Show"];
+var num = 0;
 // FUNCTIONS =================================
 // On load, create and display buttons on screen
 function renderButtons() {
@@ -34,51 +34,98 @@ $("#add-show").on("click", function(event) {
   $("#user-show").val('')
 });
 
-
-
 // On button click, run ajax and get 10 gifs
 function showGifs(){
   console.log("you clicked me");
 
   var showG = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    showG + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + showG + "&api_key=GMGkegz09o1ioCxZE030hZogINvqkb1e&limit=10";
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    
+
     $(".gifs").empty();
 
     var results = response.data
 
     for (var i = 0; i < results.length; i++) {
 
-    var gifDiv = $("<div>");
-    gifDiv.addClass("gifDiv");
+      var gifDiv = $("<div>");
+      gifDiv.addClass("gifDiv");
 
-    // Show rating under gif
-    var p = $("<p>");
-    p.text("Rating: " + results[i].rating);
+      // Show rating under gif
+      var p = $("<p>");
+      p.text("Rating: " + results[i].rating);
 
-    // Show 10 static gifs
-    var gifImg = $("<img>");
-    gifImg.attr("src", results[i].images.fixed_height_still.url);
-    gifImg.attr("data-still", results[i].images.fixed_height_still.url);
-    gifImg.attr("data-animate", results[i].images.fixed_height.url);
-    gifImg.attr("data-state", "still");
-    gifImg.addClass("gifImg");
+      // Show 10 static gifs
+      var gifImg = $("<img>");
+      gifImg.attr("src", results[i].images.fixed_height_still.url);
+      gifImg.attr("data-still", results[i].images.fixed_height_still.url);
+      gifImg.attr("data-animate", results[i].images.fixed_height.url);
+      gifImg.attr("data-state", "still");
+      gifImg.attr("data-name", showG);
+      gifImg.addClass("gifImg");
 
-    gifDiv.append(gifImg);
-    gifDiv.append(p);
-    $(".gifs").prepend(gifDiv);
+      gifDiv.append(gifImg);
+      gifDiv.append(p);
+      $(".gifs").prepend(gifDiv);
+
     };
+    $(".gifs").prepend("<hr>");
   });
 }
 
-// TODO On gif click...
+
+function showMore(event){
+  event.preventDefault();
+
+  num += 10;
+  console.log(num)
+
+  var moreGif = $(".gifImg").attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + moreGif + "&api_key=GMGkegz09o1ioCxZE030hZogINvqkb1e&limit=10&offset=" + num;
+  
+  console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+
+    var results = response.data
+
+    for (var i = 0; i < results.length; i++) {
+
+      var gifDiv = $("<div>");
+      gifDiv.addClass("gifDiv");
+
+      // Show rating under gif
+      var p = $("<p>");
+      p.text("Rating: " + results[i].rating);
+
+      // Show 10 static gifs
+      var gifImg = $("<img>");
+      gifImg.attr("src", results[i].images.fixed_height_still.url);
+      gifImg.attr("data-still", results[i].images.fixed_height_still.url);
+      gifImg.attr("data-animate", results[i].images.fixed_height.url);
+      gifImg.attr("data-state", "still");
+      gifImg.attr("data-name", moreGif);
+      gifImg.addClass("gifImg");
+
+      gifDiv.append(gifImg);
+      gifDiv.append(p);
+      $(".gifs").prepend(gifDiv);
+    };
+    $(".gifs").prepend("<hr>");
+  });
+}
+
+// On gif click...
 function animate() {
   console.log($(this).attr("data-state"));
 
@@ -104,3 +151,5 @@ $(document).on("click", ".show-btn", showGifs);
 renderButtons();
 
 $(document).on("click", ".gifImg", animate);
+
+$(document).on("click", "#showMore", showMore);
